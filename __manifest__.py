879ece431abaa0897b5d1e63551f9b910993b5e0
DoanNguyenTheVy <doannguyenthevy@gmail.com>
2023-05-26 15:12:51 +0700

    789

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Tutorial Snippet",
+
+    'summary': """
+        tutorial snippets""",
+
+    'description': """
+        ok
+    """,
+
+    'author': "vyngt",
+    'website': "https://github.com/vyngt",
+
+    'category': 'Services/Game',
+    'version': '3.9',
+    'application': 'True',
+
+    'depends': ['base', 'website'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/snippets/v_block.xml',
+        'views/snippets/snippets.xml',
+        'views/snippets/trung_block.xml',
+    ],
+}
